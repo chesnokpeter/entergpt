dae5828de3223f75b7ea863812b468781d2b2906
chesnok <ChesnokovP1107@yandex.ru>
2024-09-21 00:29:27 +0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import base64
 
+ZIP = 2
+
 client = OpenAI(
     api_key="sk-or-vv-2efcadb596a1d557d7846b906451ab3d828f50ba907a204e184afab5b927433b", # ваш ключ в VseGPT после регистрации
     base_url="https://api.vsegpt.ru/v1",
@@ -31,12 +33,13 @@ API_TOKEN = '7934869499:AAE0-tYXc5Ho7U0KWY2AbjinJaePJqpF-wA'
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
+keyboard = [
+    [InlineKeyboardButton(text="*** ЭКРАН ***", callback_data="main")],
+]
+reply_markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+
 @dp.message(Command("start"))
 async def start(message: types.Message, state: FSMContext):
-    keyboard = [
-        [InlineKeyboardButton(text="Экран", callback_data="main")],
-    ]
-    reply_markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     await message.answer('помочь?', reply_markup=reply_markup)
 
 
@@ -50,20 +53,26 @@ async def create_team(callback_query: CallbackQuery, state: FSMContext):
             # Преобразуем скриншот в изображение PIL
             img = Image.frombytes("RGB", (screenshot.width, screenshot.height), screenshot.rgb)
 
-            # Сохраняем изображение в буфер в формате JPEG
+            new_size = (img.width // ZIP, img.height // ZIP)
+            img_resized = img.resize(new_size, Image.Resampling.LANCZOS)  # Уменьшаем изображение
+
+            # Сохраняем уменьшенное изображение в буфер в формате JPEG
             buffer = BytesIO()
-            img.save(buffer, format="JPEG")
+            img_resized.save(buffer, format="JPEG")
             buffer.seek(0)
 
             # Конвертируем изображение в base64
-            img_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+        img_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+
+        await callback_query.message.answer_photo(BufferedInputFile(buffer.getvalue(), filename="screenshot.jpg"), reply_markup=reply_markup)
+
         response = client.chat.completions.create(
         model="vis-openai/gpt-4o-2024-08-06",
         messages=[
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "Напиши что ты думаешь об этой картине?"},
+                    {"type": "text", "text": "Если это картина, то напиши название картины, автора картины, эпоху и год картины. А если это фильм, то напиши оператора, режиссёра, название, год"},
                     {
                         "type": "image_url",
                         "image_url": f"data:image/jpeg;base64,{img_base64}",
@@ -73,7 +82,7 @@ async def create_team(callback_query: CallbackQuery, state: FSMContext):
             ],
             max_tokens=400,
         )
-        await callback_query.message.answer(response.choices[0].message.content)
+        await callback_query.message.answer(response.choices[0].message.content, reply_markup=reply_markup)
 
 
 async def main() -> None:
